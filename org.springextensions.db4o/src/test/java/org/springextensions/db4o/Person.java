3e1275c3ca8a4e56bc74dc16a15c301145f7c002
Oliver Lietz <info@oliverlietz.de>
2011-07-03 16:25:09 +0000

    SEDBFO-11 test-jar for shared support classes added
    --HG--
    extra : convert_revision : svn%3A1e0fb6d6-3bfa-4bec-a132-5238ddf45502/trunk%40117

--- org.springextensions.db4o/src/test/java/org/springextensions/db4o/Person.java
+++ org.springextensions.db4o/src/test/java/org/springextensions/db4o/Person.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springextensions.db4o.example;
+package org.springextensions.db4o;
 
 /**
  * @author olli
